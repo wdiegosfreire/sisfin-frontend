da616db78d109822f10aa514287f7ae7496edb58
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2022-05-31 15:50:45 -0300

    Version number update
    Version updated to 2022-05-31 1550

--- src/paginas/estrutura/EstruturaCabecalho.vue
+++ src/paginas/estrutura/EstruturaCabecalho.vue
@@ -2,7 +2,7 @@
   <v-app-bar app color="primary" dark>
     <v-app-bar-nav-icon v-if="$store.state.sessionToken" @click.stop="showNavigationDrawer = !showNavigationDrawer"></v-app-bar-nav-icon>
     <v-toolbar-title :title="computedEnviroment">
-      Financial Control System - FCS <span class="text-caption">2022-04-10 1850</span>
+      Financial Control System - FCS <span class="text-caption">2022-05-31 1550</span>
     </v-toolbar-title>
 
     <v-spacer />
