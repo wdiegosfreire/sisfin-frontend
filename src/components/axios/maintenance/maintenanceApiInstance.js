f48555883ce4b1d6c4423e2ca3392227f1508687
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2023-12-04 19:00:42 -0300

    Caminho para o backend alterado de "hard coded" para variável de ambiente.

--- src/components/axios/maintenance/maintenanceApiInstance.js
+++ src/components/axios/maintenance/maintenanceApiInstance.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const axiosInstance = axios.create({
-   baseURL: (process.env.VUE_APP_SISFIN_FRONTEND_ENVIROMENT == "default" ? "http://localhost:8080" : "http://172.31.32.253:8080")
+   baseURL: (process.env.VUE_APP_SISFIN_FRONTEND_ENVIROMENT == "default" ? "http://localhost:8080" : process.env.VUE_APP_SISFIN_FRONTEND_ENVIROMENT)
 });
 
 export default axiosInstance;
