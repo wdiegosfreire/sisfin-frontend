b712740d35acf0bd927efe68cf54c5069793b4c0
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2024-07-10 12:47:42 -0300

    Update EstruturaCabecalho.vue

--- src/paginas/estrutura/EstruturaCabecalho.vue
+++ src/paginas/estrutura/EstruturaCabecalho.vue
@@ -2,7 +2,7 @@
   <v-app-bar app color="primary">
     <v-app-bar-nav-icon v-if="$store.state.sessionToken" @click.stop="showNavigationDrawer = !showNavigationDrawer"></v-app-bar-nav-icon>
     <v-toolbar-title :title="computedEnviroment">
-      Financial Control System - FCS <span class="text-caption">2024-07-01</span>
+      Financial Control System - FCS <span class="text-caption">2024-07-10</span>
     </v-toolbar-title>
 
     <v-spacer />
