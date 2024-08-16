02891c8740e38976a7dd3c6910b93c90d90f8de2
wdiegosfreire <wdiegosfreire@yahoo.com.br>
2024-08-16 19:07:43 -0300

    86b1qk8ny: Fixed the feature "Clear" in the "Statement Patterns" module, "Statement Pattern" form.

--- src/paginas/statement/statementPattern/StatementPatternForm.vue
+++ src/paginas/statement/statementPattern/StatementPatternForm.vue
@@ -132,6 +132,8 @@ export default {
          }
 			
 			this.statementPattern.description = "";
+			this.statementPattern.location = [];
+			this.statementPattern.accountTarget = [];
       }
 	}
 };
