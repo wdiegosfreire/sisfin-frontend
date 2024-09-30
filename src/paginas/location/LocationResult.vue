1b004e333573580180d72cb43d8c00e82eb757cc
wdiegosfreire <wdiegosfreire@yahoo.com.br>
2024-09-30 19:54:18 -0300

    Identation replaced from spaces to tabs.

--- src/paginas/location/LocationResult.vue
+++ src/paginas/location/LocationResult.vue
@@ -1,40 +1,40 @@
 <template>
-   <df-result-layout :collection-length="collection.length">
-      <v-card outlined class="elevation-1 mb-4" v-for="location in collection" :key="location.identity">
-         <v-card-title>
-            <span>{{ location.name }}</span>
+	<df-result-layout :collection-length="collection.length">
+		<v-card outlined class="elevation-1 mb-4" v-for="location in collection" :key="location.identity">
+			<v-card-title>
+				<span>{{ location.name }}</span>
 
-            <v-spacer></v-spacer>
-            <v-menu offset-y>
-               <template v-slot:activator="{ on, attrs }">
-                  <v-btn outlined fab small v-bind="attrs" v-on="on"><df-icon icon="fa-bars" /></v-btn>
-               </template>
-               <v-list dense width="150">
-                  <v-subheader>Options</v-subheader>
-                  <v-list-item-group>
-                     <v-list-item @click="$emit('accessEdition', location)">
-                        <v-list-item-icon><df-icon icon="fa-pen-to-square" /></v-list-item-icon>
-                        <v-list-item-content><v-list-item-title>Edit</v-list-item-title></v-list-item-content>
-                     </v-list-item>
-                     <v-list-item @click="$emit('executeExclusion', location)">
-                        <v-list-item-icon><df-icon icon="fa-trash-can" /></v-list-item-icon>
-                        <v-list-item-content><v-list-item-title>Delete</v-list-item-title></v-list-item-content>
-                     </v-list-item>
-                  </v-list-item-group>
-               </v-list>
-            </v-menu>
-         </v-card-title>
+				<v-spacer></v-spacer>
+				<v-menu offset-y>
+					<template v-slot:activator="{ on, attrs }">
+						<v-btn outlined fab small v-bind="attrs" v-on="on"><df-icon icon="fa-bars" /></v-btn>
+					</template>
+					<v-list dense width="150">
+						<v-subheader>Options</v-subheader>
+						<v-list-item-group>
+							<v-list-item @click="$emit('accessEdition', location)">
+								<v-list-item-icon><df-icon icon="fa-pen-to-square" /></v-list-item-icon>
+								<v-list-item-content><v-list-item-title>Edit</v-list-item-title></v-list-item-content>
+							</v-list-item>
+							<v-list-item @click="$emit('executeExclusion', location)">
+								<v-list-item-icon><df-icon icon="fa-trash-can" /></v-list-item-icon>
+								<v-list-item-content><v-list-item-title>Delete</v-list-item-title></v-list-item-content>
+							</v-list-item>
+						</v-list-item-group>
+					</v-list>
+				</v-menu>
+			</v-card-title>
 
-         <v-divider></v-divider>
+			<v-divider></v-divider>
 
-         <v-card-text>
-            <df-grid>
-               <df-output-text class="text-left" label="Identity">{{ location.identity }}</df-output-text>
-               <df-output-text class="text-left" label="Notes">{{ location.note ? location.note : "No notes" }}</df-output-text>
-            </df-grid>
-         </v-card-text>
-      </v-card>
-   </df-result-layout>
+			<v-card-text>
+				<df-grid>
+					<df-output-text class="text-left" label="Identity">{{ location.identity }}</df-output-text>
+					<df-output-text class="text-left" label="Notes">{{ location.note ? location.note : "No notes" }}</df-output-text>
+				</df-grid>
+			</v-card-text>
+		</v-card>
+	</df-result-layout>
 </template>
 
 <script>
@@ -45,21 +45,21 @@ import DfIcon from "../../components/df-icon/Icon.vue";
 import DfOutputText from "../../components/output/OutputText.vue";
 
 export default {
-   name: "LocationResult",
+	name: "LocationResult",
 
-   components: { DfResultLayout, DfGrid, DfOutputText, DfIcon },
+	components: { DfResultLayout, DfGrid, DfOutputText, DfIcon },
 
-   props: {
-      collection: {
-         type: Array,
-         required: true
-      }
-   },
+	props: {
+		collection: {
+			type: Array,
+			required: true
+		}
+	},
 
-   data() {
-      return {
-         search: ""
-      };
-   }
+	data() {
+		return {
+			search: ""
+		};
+	}
 };
 </script>
