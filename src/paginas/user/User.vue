f2ef21593ab4b49af711fefdfcf9d63383b19764
wdiegosfreire <wdiegosfreire@yahoo.com.br>
2025-05-26 16:04:24 -0300

    Added "Reset Password" and "New Account" buttons in the login page.

--- src/paginas/user/User.vue
+++ src/paginas/user/User.vue
@@ -1,30 +1,38 @@
 <template>
-   <v-card max-width="500">
-        <v-img src="../../assets/imagens/financas-pessoais.jpg">
-            <v-card-title class="headline">Authentication</v-card-title>
-        </v-img>
+   <div>
+      <v-card max-width="500">
+           <v-img src="../../assets/imagens/financas-pessoais.jpg">
+               <v-card-title class="headline">Authentication</v-card-title>
+           </v-img>
+   
+           <v-card-text>
+               <v-text-field label="E-mail" v-model="user.email" append-icon="mdi-mail" />
+               <v-text-field label="Password" type="password" v-model="password" append-icon="mdi-lock" />
+           </v-card-text>
+   
+           <v-card-actions>
+               <v-btn color="button" width="150" @click="executeAuthentication">Confirm</v-btn>
+               <v-btn color="button" width="150" @click="resetForm">Clear</v-btn>
+           </v-card-actions>
+   
+            <v-card-actions v-if="maintenance">
+               <v-alert outlined prominent type="error" border="left" class="text-left">
+                  The service "MAINTENANCE" is not responding. We'll try again after each 30 seconds until the service is back again.
+               </v-alert>
+            </v-card-actions>
+            <v-card-actions v-if="transaction">
+               <v-alert outlined prominent type="error" border="left" class="text-left">
+                  The service "TRANSACTION" is not responding. We'll try again after each 30 seconds until the service is back again.
+               </v-alert>
+            </v-card-actions>
 
-        <v-card-text>
-            <v-text-field label="E-mail" v-model="user.email" append-icon="mdi-mail" />
-            <v-text-field label="Password" type="password" v-model="password" append-icon="mdi-lock" />
-        </v-card-text>
+            <v-card-text class="text-right">
+               <v-btn @click="accessResetPassword" color="primary" plain>Forgot your password?</v-btn>
+            </v-card-text>
+       </v-card>
 
-        <v-card-actions>
-            <v-btn color="button" width="150" @click="executeAuthentication">Confirm</v-btn>
-            <v-btn color="button" width="150" @click="resetForm">Clear</v-btn>
-        </v-card-actions>
-
-         <v-card-actions v-if="maintenance">
-            <v-alert outlined prominent type="error" border="left" class="text-left">
-               The service "MAINTENANCE" is not responding. We'll try again after each 30 seconds until the service is back again.
-            </v-alert>
-         </v-card-actions>
-         <v-card-actions v-if="transaction">
-            <v-alert outlined prominent type="error" border="left" class="text-left">
-               The service "TRANSACTION" is not responding. We'll try again after each 30 seconds until the service is back again.
-            </v-alert>
-         </v-card-actions>
-    </v-card>
+       <v-btn @click="accessRegistration" class="mt-5" color="primary" plain>Don't have an account? Sign up and get started!</v-btn>
+   </div>
 </template>
 
 <script>
